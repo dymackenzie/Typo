d595276d4b1c8536ac67fb3c8d02367380ae7338
kenzie <kenziedy@students.cs.ubc.ca>
2023-12-25 17:54:05 -0800

    player flips horizontally

--- src/scripts/player.cs
+++ src/scripts/player.cs
@@ -8,19 +8,24 @@ public partial class player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		AnimatedSprite2D sprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
 		Vector2 direction = Input.GetVector("a_left", "d_right", "w_up", "s_down");
 		if (direction == Vector2.Zero) {
 			playerState = "idle";
 		} else if (direction != Vector2.Zero && direction.X > 0) {
+			// flips the sprite horizontally
+			sprite2D.FlipH = false;
 			playerState = "run";
 		} else if (direction != Vector2.Zero && direction.X < 0) {
+			// flips the sprite horizontally
+			sprite2D.FlipH = true;
 			playerState = "run";
 		}
 
 		Velocity = direction * SPEED;
 		MoveAndSlide();
-		GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play(playerState);
+		sprite2D.Play(playerState);
 	}
 
 }
