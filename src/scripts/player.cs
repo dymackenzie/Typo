d00553844006c53ebcc258a21a7aa803503f816d
kenzie <kenziedy@students.cs.ubc.ca>
2023-12-28 00:07:21 -0800

    added friction and acceleration

--- src/scripts/player.cs
+++ src/scripts/player.cs
@@ -5,13 +5,21 @@ public partial class player : CharacterBody2D
 {
 	// movement variables
 	public const float SPEED = 100.0f;
+	public const float FRICTION = 0.8f;
+	public const float ACCELERATION = 0.8f;
+
 
 	// animation variable
 	public string PLAYER_STATE;
+	private bool isJumping;
+	private bool isFalling;
 
 	private AnimatedSprite2D sprite2D;
 	private CollisionShape2D hitbox;
 
+	/*
+	Constructor
+	*/
     public override void _Ready()
     {
 		PLAYER_STATE = "idle";
@@ -19,31 +27,51 @@ public partial class player : CharacterBody2D
 		hitbox = GetNode<CollisionShape2D>("hitbox");
     }
 
+	/*
+	Called every frame, "delta" is amount of time passed.
+	*/
     public override void _PhysicsProcess(double delta)
 	{
-		float x = Input.GetActionStrength("d_right") - Input.GetActionStrength("a_left");
-		float y = (Input.GetActionStrength("s_down") - Input.GetActionStrength("w_up")) / 2;
-		Vector2 direction = new(x, y);
+		Vector2 direction = Input.GetVector("a_left", "d_right", "w_up", "s_down");
 		direction = direction.Normalized();
+		float x = direction.X;
+		float y = direction.Y / 2;
 
 		if (Input.IsActionJustPressed("jump_space")) {
-			
+
 		}
 
 		PLAYER_STATE = "run";
-		if (direction == Vector2.Zero) {
+		if (x == 0 && y == 0) {
+			// slowdown when no input
+			x = Lerp(Velocity.X, 0, FRICTION);
+			y = Lerp(Velocity.Y, 0, FRICTION);
 			PLAYER_STATE = "idle";
-		} else if (direction != Vector2.Zero && direction.X > 0) {
-			// flips the sprite horizontally
-			sprite2D.FlipH = false;
-		} else if (direction != Vector2.Zero && direction.X < 0) {
-			// flips the sprite horizontally
-			sprite2D.FlipH = true;
+		} else if (x != 0 || y != 0) {
+			if (x > 0) {
+				// if player is going right
+				// flips the sprite horizontally
+				sprite2D.FlipH = false;
+			} else {
+				// if player is going left
+				// flips the sprite horizontally
+				sprite2D.FlipH = true;
+			}
+			// accelerates when input
+			x = Lerp(Velocity.X, x * SPEED, ACCELERATION);
+			y = Lerp(Velocity.Y, y * SPEED, ACCELERATION);
 		}
 
-		Velocity = direction * SPEED;
+		Velocity = new Vector2(x, y);
 		MoveAndSlide();
 		sprite2D.Play(PLAYER_STATE);
+	}
+
+	/*
+	Linear interpolation function.
+	*/
+	private float Lerp(float first, float second, float by) {
+		return first + ((second - first) * by);
 	}
 
 }
