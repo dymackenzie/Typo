ebaf0096e2eee4780024ebdaa3493201ad55bf0f
Mackenzie <kenziedy@students.cs.ubc.ca>
2025-01-16 00:14:54 -0800

    words ~ updated wordbank

--- src/scripts/python/wordlist_manipulator.py
+++ src/scripts/python/wordlist_manipulator.py
@@ -1,13 +1,18 @@
 import os
 
-path = r"C:/Users/macke/Downloads/wordlist.10000.txt"
-dest = r"C:/Users/macke/Downloads/5_letters.txt"
+path = r"C:/Users/macke/Downloads/google-10000-english.txt"
+dest = r"C:/Users/macke/Downloads/4_letters.txt"
 
 with open(path, 'r') as fpIn, open(dest, 'w') as fpOut:
     lineNumber = 0
+    lineBreak = 0
     for line in fpIn:
         lineNumber += 1
+        lineBreak += 1
         line = line.rstrip()
-        if (len(line) == 5 and lineNumber % 10 == 0):
-            fpOut.write(("\"{}\", ").format(line))
+        if (len(line) == 4 and lineNumber % 2 == 0):
+            if (lineBreak % 10 == 0):
+                fpOut.write(("\"{}\",\n").format(line))
+            else:
+                fpOut.write(("\"{}\", ").format(line))
 
